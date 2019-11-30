0994efe2c7994061f7ef4dfb79afaacc9a1a40fe
Christian Koestlin <christian.koestlin@esrlabs.com>
2019-11-30 19:26:51 +0100

    Import to git

---
+++ lib/jenkins/rest/client.rb
@@ -0,0 +1,54 @@
+require "jenkins/rest/client/version"
+require "json"
+
+require "faraday"
+
+module Jenkins
+  module Rest
+    class Client
+      attr_reader :server
+      def initialize(server:, username:, password:)
+        @server = Faraday.new(server) do |connection|
+          connection.basic_auth(username, password)
+          connection.adapter(Faraday.default_adapter)
+        end
+      end
+      def job(job)
+        return Job.new(JSON.parse(@server.get("/job/#{job}/api/json").body), @server)
+      end
+    end
+
+    class Artifact < OpenStruct
+      def initialize(json, build, server)
+        super(json)
+        @build = build
+        @server = server
+      end
+      def get
+        artifact_url = "#{@build.url}/artifact/#{relativePath}"
+        return @server.get(artifact_url).body
+      end
+    end
+
+    class Build < OpenStruct
+      def initialize(json, server)
+        super(json)
+        @server = server
+      end
+      def artifacts
+        return JSON.parse(@server.get("#{url}/api/json").body)["artifacts"].map{|i|Artifact.new(i, self, @server)}
+      end
+    end
+
+    class Job < OpenStruct
+      def initialize(json, server)
+        super(json)
+        @server = server
+      end
+      def last_successful_build
+        content = JSON.parse(@server.get("#{url}/api/json").body)
+        return Build.new(content["lastSuccessfulBuild"], @server)
+      end
+    end
+  end
+end
---
+++ spec/jenkins/rest/client_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe Jenkins::Rest::Client do
+  it "has a version number" do
+    expect(Jenkins::Rest::Client::VERSION).not_to be nil
+  end
+end
